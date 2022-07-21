3c07f280bfe605b9cf2d6df6a954623fe7ae8564
elystanz <elysestanziale@gmail.com>
2022-07-21 19:56:01 -0400

    heroku fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,13 +8,15 @@ import Inspo from '../assets/inspo-co.png';
 
 function Main() {
   return (
-    <section className="portfolio row">
+    <section >
+            <img className="inpso-co column" src={ Inspo } alt="Inspo.co logo" href="https://inspo-co.herokuapp.com/"/>
 
+<div className="portfolio row">
       <img className="horiseon column" src={ Horiseon } alt="Horiseon web application in use"/>
       <img className="budget-tracker column" src={ Budget } alt="Budget Tracker web application in use"/>
       <img className="work-day column" src={ workDay } alt="Work Day Scheduler web application in use"/>
       <img className="password-gen column" src={ passGen } alt="Password Generator web application in use"/>
-      <img className="password-gen column" src={ Inspo } alt="Inspo.co logo" href="https://inspo-co.herokuapp.com/"/>
+      </div>
 
     </section>
   );
